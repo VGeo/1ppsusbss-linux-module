e8f82c5470e2713961ede064e0a7b7deebc792c3
VGeo <vgeo89@gmail.com>
2018-02-21 00:04:30 +0300

    Remove usb_class registration and file_operations - we no longer need them. Copy PPS time initialization, calibration and event handling from pps_gen_parport.c and connect them to USB functions. We will have to use usb_submit_urb() instead of usb_bulk_msg() in atomic context.

--- pps-cfx3.c
+++ pps-cfx3.c
@@ -1,170 +1,210 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/usb.h>
-#include <linux/uaccess.h>
+#include <linux/timer.h>
+#include <linux/hrtimer.h>
 
 #define BULK_EP_OUT 0x01
 #define BULK_EP_IN 0x81
 #define MAX_PKT_SIZE 512
 #define MIN(a,b) (((a) <= (b)) ? (a) : (b))
 
-static struct usb_device *device;
-static struct usb_class_driver class;
-//static unsigned char bulk_buf[MAX_PKT_SIZE];
+#define SEND_DELAY_MAX		100000
 
-static int pps_cfx3_open(struct inode *i, struct file *f)
-{
-  	printk("PPS open was called\n");
-    return 0;
-}
-static int pps_cfx3_close(struct inode *i, struct file *f)
-{
-  	printk("PPS close was called\n");
-    return 0;
-}
-static ssize_t pps_cfx3_read(struct file *f, char __user *buf, size_t cnt, loff_t *off)
-{
-  	int retval;
-	int read_cnt;
-	static unsigned char *bulk_buf;
+static unsigned int send_delay = 30000;
+MODULE_PARM_DESC(delay,
+	"Delay between setting and dropping the signal (ns)");
+module_param_named(delay, send_delay, uint, 0);
 
-	bulk_buf = kzalloc(sizeof(char) * MAX_PKT_SIZE, GFP_KERNEL);
-	if (!bulk_buf) {
-	  	printk("cfx3 read: cannot allocate buf\n");
-		return -ENOMEM;
-	}
 
-	/* Read the data from the bulk endpoint */
-	retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN),
-			bulk_buf, MAX_PKT_SIZE, &read_cnt, 5000);
-	if (retval)
-	{
-		printk(KERN_ERR "Bulk message returned %d\n", retval);
-		goto err;
-	}
-	printk("PPS read bulk_buf[0] = 0x%x, cnt = %d\n", bulk_buf[0], read_cnt);
-	if (copy_to_user(buf, bulk_buf, MIN(cnt, read_cnt)))
-	{
-		retval = -EFAULT;
-		goto err;
-	}
+#define SAFETY_INTERVAL	3000	/* set the hrtimer earlier for safety (ns) */
 
-  	printk("PPS read was called: %lu bytes\n", cnt);
-	kfree(bulk_buf);
-	return MIN(cnt, read_cnt);
+/* internal per port structure */
+struct pps_generator_cyfx3 {
+	struct usb_device *usbd;	/* USB device */
+	struct hrtimer timer;
+	int attached;
 
-err:
-	kfree(bulk_buf);
-	return retval;
+	/* calibrated time between a hrtimer event and the reaction */
+	long hrtimer_error;
 
-}
-
-static ssize_t pps_cfx3_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off)
-{
-	int retval;
-	int wrote_cnt = MIN(cnt, MAX_PKT_SIZE);
-	static unsigned char *bulk_buf;
-
-	bulk_buf = kzalloc(sizeof(char) * MAX_PKT_SIZE, GFP_KERNEL);
-	if (!bulk_buf) {
-	  	printk("cfx3 write: cannot allocate buf\n");
-		return -ENOMEM;
-	}
-
-	if (copy_from_user(bulk_buf, buf, MIN(cnt, MAX_PKT_SIZE)))
-	{
-		retval = -EFAULT;
-		goto err;
-	}
-	printk("PPS write bulk_buf[0] = 0x%x, cnt = %d\n", bulk_buf[0], wrote_cnt);
-
-	/* Write the data into the bulk endpoint */
-	retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT),
-			bulk_buf, MIN(cnt, MAX_PKT_SIZE), &wrote_cnt, 5000);
-	if (retval)
-	{
-		printk(KERN_ERR "Bulk message returned %d\n", retval);
-		goto err;;
-	}
-
-  	printk("PPS write was called: %lu bytes\n", cnt);
-	kfree(bulk_buf);
-	return wrote_cnt;
-
-err:
-	kfree(bulk_buf);
-	return retval;
-}
-
-static struct file_operations fops =
-{
-    .open = pps_cfx3_open,
-    .release = pps_cfx3_close,
-    .read = pps_cfx3_read,
-    .write = pps_cfx3_write,
+	long port_write_time;		/* calibrated port write time (ns) */
 };
 
-//static int send_byte(unsigned char b)
-//{
-//  	int retval;
-//	int wrote_cnt = MAX_PKT_SIZE;
-//
-//	memset(bulk_buf, 0, MAX_PKT_SIZE);
-//	bulk_buf[0] = b;
-//	bulk_buf[1] = b;
-//	bulk_buf[2] = b;
-//	bulk_buf[3] = b;
-//	printk(KERN_INFO "Write byte 0x%x\n", bulk_buf[0]);
-//	retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT),
-//	    bulk_buf, MAX_PKT_SIZE, &wrote_cnt, 3000);
-//	if (retval)
-//	{
-//	  	printk( KERN_INFO "Send retval is %d\n", retval);
-//	}
-//	printk(KERN_INFO "wrote_cnt is %d\n", wrote_cnt);
-//
-//	return 0;	
-//}
+static int pps_write(struct usb_device *usbd, u8 value)
+{
+  	int ret = 0;
+	int wrote_cnt = 0;
+/*	ret = usb_bulk_msg(usbd, usb_sndbulkpipe(usbd, BULK_EP_OUT),
+			&value, sizeof(value), &wrote_cnt, 5000);
+	dev_info(&usbd->dev, "wr %d, %d\n", wrote_cnt, ret);*/
+	return ret;
+}
 
-//static int recv_byte(void)
-//{
-//  	int retval;
-//	int read_cnt = MAX_PKT_SIZE;
-//	int tmp = 0xcccccccc;
-//
-//	retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN),
-//	    &tmp, MAX_PKT_SIZE, &read_cnt, 30000);
-//	if (retval)
-//	{
-//	  	printk( KERN_ERR "Recv retval is %d\n", retval);
-//	}
-//	printk(KERN_INFO "read_cnt is %d\n", read_cnt);
-//
-//	return tmp;	
-//}
+/* the kernel hrtimer event */
+static enum hrtimer_restart hrtimer_event(struct hrtimer *timer)
+{
+	struct timespec expire_time, ts1, ts2, ts3, dts;
+	struct pps_generator_cyfx3 *gen;
+	struct usb_device *usbd;
+	long lim, delta;
+	unsigned long flags;
+
+	/* We have to disable interrupts here. The idea is to prevent
+	 * other interrupts on the same processor to introduce random
+	 * lags while polling the clock. getnstimeofday() takes <1us on
+	 * most machines while other interrupt handlers can take much
+	 * more potentially.
+	 *
+	 * NB: approx time with blocked interrupts =
+	 * send_delay + 3 * SAFETY_INTERVAL
+	 */
+	local_irq_save(flags);
+
+	/* first of all we get the time stamp... */
+	getnstimeofday(&ts1);
+	expire_time = ktime_to_timespec(hrtimer_get_softexpires(timer));
+	gen = container_of(timer, struct pps_generator_cyfx3, timer);
+	usbd = gen->usbd;
+	lim = NSEC_PER_SEC - send_delay - gen->port_write_time;
+
+	/* check if we are late */
+	if (expire_time.tv_sec != ts1.tv_sec || ts1.tv_nsec > lim) {
+		local_irq_restore(flags);
+		pr_err("we are late this time %ld.%09ld\n",
+				ts1.tv_sec, ts1.tv_nsec);
+		goto done;
+	}
+
+	/* busy loop until the time is right for an assert edge */
+	do {
+		getnstimeofday(&ts2);
+	} while (expire_time.tv_sec == ts2.tv_sec && ts2.tv_nsec < lim);
+
+	/* set the signal */
+	//USB BULK WRITE 1
+	pps_write(usbd, 1);
+
+	/* busy loop until the time is right for a clear edge */
+	lim = NSEC_PER_SEC - gen->port_write_time;
+	do {
+		getnstimeofday(&ts2);
+	} while (expire_time.tv_sec == ts2.tv_sec && ts2.tv_nsec < lim);
+
+	/* unset the signal */
+	//USB BULK WRITE 0
+	pps_write(usbd, 0);
+	printk(KERN_CRIT "low\n");
+
+	getnstimeofday(&ts3);
+
+	local_irq_restore(flags);
+
+	/* update calibrated port write time */
+	dts = timespec_sub(ts3, ts2);
+	gen->port_write_time =
+		(gen->port_write_time + timespec_to_ns(&dts)) >> 1;
+
+done:
+	/* update calibrated hrtimer error */
+	dts = timespec_sub(ts1, expire_time);
+	delta = timespec_to_ns(&dts);
+	/* If the new error value is bigger then the old, use the new
+	 * value, if not then slowly move towards the new value. This
+	 * way it should be safe in bad conditions and efficient in
+	 * good conditions.
+	 */
+	if (delta >= gen->hrtimer_error)
+		gen->hrtimer_error = delta;
+	else
+		gen->hrtimer_error = (3 * gen->hrtimer_error + delta) >> 2;
+
+	/* update the hrtimer expire time */
+	hrtimer_set_expires(timer,
+			ktime_set(expire_time.tv_sec + 1,
+				NSEC_PER_SEC - (send_delay +
+				gen->port_write_time + SAFETY_INTERVAL +
+				2 * gen->hrtimer_error)));
+
+	return HRTIMER_RESTART;
+}
+
+/* calibrate port write time */
+#define PORT_NTESTS_SHIFT	5
+static void calibrate_port(struct pps_generator_cyfx3 *gen)
+{
+	struct usb_device *usbd = gen->usbd;
+	int i;
+	long acc = 0;
+
+	for (i = 0; i < (1 << PORT_NTESTS_SHIFT); i++) {
+		struct timespec a, b;
+		unsigned long irq_flags;
+
+		local_irq_save(irq_flags);
+		getnstimeofday(&a);
+		//port->ops->write_control(port, NO_SIGNAL);
+		//USB BULK WRITE 0
+		pps_write(usbd, 0);
+		getnstimeofday(&b);
+		local_irq_restore(irq_flags);
+
+		b = timespec_sub(b, a);
+		acc += timespec_to_ns(&b);
+	}
+
+	gen->port_write_time = acc >> PORT_NTESTS_SHIFT;
+	pr_info("port write takes %ldns\n", gen->port_write_time);
+}
+
+static inline ktime_t next_intr_time(struct pps_generator_cyfx3 *gen)
+{
+	struct timespec ts;
+
+	getnstimeofday(&ts);
+
+	return ktime_set(ts.tv_sec +
+			((ts.tv_nsec > 990 * NSEC_PER_MSEC) ? 1 : 0),
+			NSEC_PER_SEC - (send_delay +
+			gen->port_write_time + 3 * SAFETY_INTERVAL));
+}
 
 static int pps_cfx3_probe(struct usb_interface *interface, const struct usb_device_id *id)
 {
-  	int retval, tmp;
+	struct usb_device *usbd = NULL;
+	struct pps_generator_cyfx3 *gen = NULL;
 
-	device = interface_to_usbdev(interface);
-	class.name = "usb/pps_cfx3%d";
-	class.fops = &fops;
-	if ((retval = usb_register_dev(interface, &class)) < 0)
-	{
-        	/* Something prevented us from registering this driver */
-        	printk(KERN_INFO "Not able to get a minor for this device.");
-	}
-	else
-	{
-		printk(KERN_INFO "Minor obtained: %d\n", interface->minor);
-	}
-	return retval;
+	usbd = interface_to_usbdev(interface);
+
+	gen = kzalloc(sizeof(*gen), GFP_KERNEL);
+	if (!gen)
+		return -ENOMEM;
+	dev_info(&usbd->dev, "allocate generator at 0x%p\n", gen);
+
+	gen->usbd = usbd;
+	dev_set_drvdata(&usbd->dev, gen);
+	
+	gen->hrtimer_error = SAFETY_INTERVAL;
+	dev_info(&usbd->dev, "attached to usb device %d\n", usbd->devnum);
+	gen->attached = 1;
+
+	calibrate_port(gen);
+
+	hrtimer_init(&gen->timer, CLOCK_REALTIME, HRTIMER_MODE_ABS);
+	gen->timer.function = hrtimer_event;
+	hrtimer_start(&gen->timer, next_intr_time(gen), HRTIMER_MODE_ABS);
+	
+	return 0;
 }
  
 static void pps_cfx3_disconnect(struct usb_interface *interface)
 {
-  	usb_deregister_dev(interface, &class);
+  	struct usb_device *usbd = interface_to_usbdev(interface);
+	struct pps_generator_cyfx3 *gen = dev_get_drvdata(&usbd->dev);
+	hrtimer_cancel(&gen->timer);
+	dev_info(&usbd->dev, "free generator at 0x%p\n", gen);
+	kfree(gen);
+  	//usb_deregister_dev(interface, &class);
 	printk(KERN_INFO "Cfx3 pps drive removed\n");
 }
  
@@ -185,21 +225,9 @@ static struct usb_driver pps_cfx3_driver =
 
 static int __init pps_cfx3_init(void)
 {
-  	int tmp = 0, ret = 0;
+  	int ret = 0;
 	ret = usb_register(&pps_cfx3_driver);
 	printk( KERN_INFO "usb_register: %d\n", ret);
-	/* Do test blink 
-	msleep(2000);
-	send_byte(0);
-	msleep(1000);
-	send_byte(0xff);
-	msleep(1000);
-	send_byte(0);
-	msleep(1000);
-	send_byte(0xff);
-	msleep(1000);
-	tmp = recv_byte();
-	printk(KERN_INFO "Recv 0x%x\n", tmp);*/
 	return ret;
 }
 
